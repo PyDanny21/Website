5ba66773da6422b3d4c5ba0d5339c098b83b10fc
Daniel Quansah <114119228+PyDanny21@users.noreply.github.com>
2022-10-28 18:44:25 -0700

    Add files via upload

---
+++ trial.js
@@ -0,0 +1,7 @@
+
+function toggleBtn(){
+    const image2=document.getElementById('image2')
+    const btne=document.getElementById('btn');
+    btne.classList.toggle('active');
+    image2.classList.toggle('on')
+}
